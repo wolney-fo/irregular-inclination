ba95e2a3f7220b02b5b55e9c36f1570709e8320c
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2023-05-28 16:23:41 -0300

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center text-white bg-zinc-900 py-20">
+      <span>
+        Made with 💜 <br />
+        by{" "}
+        <a
+          href="https://bio.link/wolneyol"
+          className="text-violet-500 hover:underline"
+        >
+          @wolney-fo
+        </a>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
